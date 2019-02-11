7e9a30230a92e290c48649c9ed79e4570afb4fd6
AlexIsOnFire6 <47330259+AlexIsOnFire6@users.noreply.github.com>
2019-02-11 10:22:42 -0600

    eCH

--- cs30-p5js-template-master/sketch.js
+++ cs30-p5js-template-master/sketch.js
@@ -5,30 +5,11 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-
-let x,y;
-let xSpeed,ySpeed;
-
-
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  x = width/2;
-  y = height/2;
-  xSpeed = random(-3, 3)
-  ySpeed = random(-3, 3)
+  createCanvas(400, 400);
 }
-
 function draw() {
-  //background(0, 0, 0);
-  ellipse(x, y, 25, 25);
-  x += xSpeed;
-  y += ySpeed;
-  fill(random(250), random(250), random(250))
-}
+  background(200, 0, 200);
 
-function mousePressed(){
-  //fill(255,0,0)
-  xSpeed = random(-3, 3)
-  ySpeed = random(-3, 3)
+  rect(100, 50, 25, 75)
 }
- 
