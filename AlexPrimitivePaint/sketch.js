b21c5ba4beda131f3c10549b0d1587b52a4fc59a
AlexIsOnFire6 <47330259+AlexIsOnFire6@users.noreply.github.com>
2019-02-25 10:59:21 -0600

    Demo 2
    Fade in and out

--- AlexPrimitivePaint/sketch.js
+++ AlexPrimitivePaint/sketch.js
@@ -10,32 +10,31 @@ function setup() {
 // Just A Canvas
 
 function draw() {
-  //background(0, 0, 0);
-  keyTyped();
+  background(0, 0, 0);
   textSize(50);
   textFont("Georgia");
   text("Alex Delorme", 50, 100);
   fill(random(110), random(10), random(204));
+  arc(1000, 1000, random(255), random(255), random(255), random(255), random(255), PI + QUARTER_PI, PIE);
 }
 //Background and name information
 
 function keyTyped(){
   if (key === "a") {
-    fill(112, 40, 255);
+    fill(random(255), random(255), random(255));
     rect(mouseX, mouseY, 50, 100);
   }
   if (key === "s") {
     fill(112, 40, 255);
-    ellipse(mouseX, mouseY, 200, 100);
+    ellipse(random(255), random(255), random(255));
   }
   if (key === "d"){
-    fill(112, 40, 255);
+    fill(random(255), random(255), random(255));
     quad(mouseX, mouseY, mouseX -25, mouseY +25, mouseX, mouseY +50, mouseX +25, mouseY +25);
   }
   if(key === " "){
     background(0);
   }
-  arc(random(50), random(50), random(80), random(80), random(0), PI + QUARTER_PI, PIE);
 }
 //code that makes the key's function when press
 //a animated arc for kicks and criteriaS
